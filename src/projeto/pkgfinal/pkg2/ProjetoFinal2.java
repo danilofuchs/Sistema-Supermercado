92fba62b68fba2aaaefac35d19744e4eb2242581
danil <danil@NOTE-DANILO>
2017-10-18 15:14:15 -0200

    Adicionando classes iniciais

---
+++ src/projeto/pkgfinal/pkg2/ProjetoFinal2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projeto.pkgfinal.pkg2;
+
+/*  ---------------------------
+ * | Package: projeto.pkgfinal.pkg2
+ * | Classe:  ProjetoFinal2
+ * | Autor:   danil
+ * | __________________________
+ */
+
+public class ProjetoFinal2 {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
