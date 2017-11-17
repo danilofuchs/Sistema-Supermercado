94df73e5cabb26758f1b034f9dbdad87bb39ba07
danil <danil@192.168.10.24>
2017-11-17 15:09:16 -0200

    Chamar VendaGUI no main (para testes)

--- src/projeto/pkgfinal/pkg2/SistemaSupermercadoMain.java
+++ src/projeto/pkgfinal/pkg2/SistemaSupermercadoMain.java
@@ -1,10 +1,12 @@
 package projeto.pkgfinal.pkg2;
+import interfaceGrafica.VendaGUI;
+import exceptions.NegativeStockException;
 
 public class SistemaSupermercadoMain {
 
     public static void main(String[] args) {
-
-        
+	VendaGUI window = new VendaGUI();
+        window.setVisible(true);
     }
 
 }
