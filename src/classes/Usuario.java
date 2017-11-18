65190677e4e958606495f8cdd3700b0a97927629
danil <danil@192.168.10.24>
2017-11-18 18:06:38 -0200

    Adicionada classe usuário (com verificação hash)

---
+++ src/classes/Usuario.java
@@ -0,0 +1,74 @@
+package classes;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Usuario {
+
+    private String nome;
+    private byte[] senha;
+    private String cargo;
+    
+    private MessageDigest md;
+
+    public Usuario(String nome, String senha, String cargo) {
+	this.nome = nome;
+	this.cargo = cargo;
+	try {
+	    md = MessageDigest.getInstance("SHA-256");
+	    try {
+		this.senha = md.digest(senha.getBytes("UTF-8"));
+	    }
+	    catch (UnsupportedEncodingException ex) {
+		Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+	    }
+	}
+	catch (NoSuchAlgorithmException ex) {
+	    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }
+
+    public boolean checkPassword(String senha) {
+	byte[] hash;
+	boolean result = false;
+
+	try {
+	    hash = md.digest(senha.getBytes("UTF-8"));
+	    if (hash == this.senha) {
+		result = true;
+	    }
+	}
+	catch (UnsupportedEncodingException ex) {
+	    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+	}
+	
+	return result;
+    }
+    
+    public void setSenha(String senha) {
+	try {
+	    this.senha = md.digest(senha.getBytes("UTF-8"));
+	} catch (UnsupportedEncodingException ex) {
+	    Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+	}
+    }
+    
+    public void setNome(String nome) {
+	this.nome = nome;
+    }
+    
+    public String getNome() {
+	return nome;
+    }
+
+    public String getCargo() {
+	return cargo;
+    }
+
+    public void setCargo(String cargo) {
+	this.cargo = cargo;
+    }
+}
