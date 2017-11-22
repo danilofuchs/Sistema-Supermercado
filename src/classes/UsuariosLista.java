43bfdb62d97e16206db368f7ca6074035966639e
DAELN <DAELN@10.3.2.154>
2017-11-22 17:19:04 -0200

    Consertado erro de lógica em getUsuariosLista

--- src/classes/UsuariosLista.java
+++ src/classes/UsuariosLista.java
@@ -22,10 +22,10 @@ import java.util.logging.Logger;
  * @author DAELN
  */
 public class UsuariosLista {
-    
+
     String filename = "Users.txt";
-    ArrayList<Usuario> usuarios;
-    
+    ArrayList<Usuario> usuarios = new ArrayList();
+
     public UsuariosLista() {
         try {
             getUsuariosArquivo();
@@ -38,7 +38,7 @@ public class UsuariosLista {
             }
         }
     }
-    
+
     public void close() {
         try {
             gravarUsuariosArquivo();
@@ -46,69 +46,75 @@ public class UsuariosLista {
             Logger.getLogger(UsuariosLista.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public void addUsuario(String nome, String senha, String cargo) throws UsernameNotUniqueException {
         boolean unique = true;
-        for (Usuario u : usuarios) {
-            if (nome.equals(nome)) {
-                unique = false;
-                throw new UsernameNotUniqueException("Nome de usuário já existente, por favor selecione outro");
+        if (usuarios.size() >= 1) {
+            for (Usuario u : usuarios) {
+                if (nome.equals(nome)) {
+                    unique = false;
+                    throw new UsernameNotUniqueException("Nome de usuário já existente, por favor selecione outro");
+                }
             }
         }
         if (unique) {
             usuarios.add(new Usuario(nome, senha, cargo));
         }
     }
-    
+
     public void removeUsuario(String nome, String senha) throws PasswordInvalidException, UsernameNotFoundException {
         boolean found = false;
-        for (Usuario u : usuarios) {
-            if (nome.equals(nome)) {
-                found = true;
-                if (u.checkPassword(senha) == true) {
-                    usuarios.remove(u);
-                    
-                } else {
-                    throw new PasswordInvalidException("Senha inválida. Usuário não removido.");
+        if (usuarios.size() >= 1) {
+            for (Usuario u : usuarios) {
+                if (nome.equals(nome)) {
+                    found = true;
+                    if (u.checkPassword(senha) == true) {
+                        usuarios.remove(u);
+
+                    } else {
+                        throw new PasswordInvalidException("Senha inválida. Usuário não removido.");
+                    }
+                    break;
                 }
-                break;
             }
         }
         if (!found) {
             throw new UsernameNotFoundException("Usuário não cadastrado.");
         }
+
     }
-    
-    
+
     private void getUsuariosArquivo() throws FileNotFoundException, IOException {
         FileReader file = new FileReader(filename);
         BufferedReader readFile = new BufferedReader(file);
 
-        String input = readFile.readLine();
+        String input = "";
         String[] usuario;
         while (input != null) {
             input = readFile.readLine();
-            usuario = input.split(" ; ");
-            usuarios.add(new Usuario(usuario[0], usuario[1], usuario[2]));
+            if (input != null) {
+                usuario = input.split(" ; ");
+                usuarios.add(new Usuario(usuario[0], usuario[1], usuario[2]));
+            }
         }
         readFile.close();
 
     }
-    
+
     public void gravarUsuariosArquivo() throws IOException {
         FileWriter file = new FileWriter(filename);
         PrintWriter writeFile = new PrintWriter(file);
-        
+
         for (Usuario u : usuarios) {
             writeFile.println(String.format("%s ; %s ; %s", u.getNome(), u.getSenha(), u.getCargo()));
         }
         writeFile.close();
     }
-    
+
     public void inicializarArquivo() throws IOException {
         FileWriter file = new FileWriter(filename);
         PrintWriter writeFile = new PrintWriter(file);
-        
+
         writeFile.write("");
         writeFile.close();
     }
