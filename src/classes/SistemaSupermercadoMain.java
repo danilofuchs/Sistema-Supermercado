45144793e3fc9a626c38364325d14d54fd020aaf
DAELN <DAELN@10.3.2.154>
2017-11-22 17:23:03 -0200

    Adicionado usuário de teste no main

--- src/classes/SistemaSupermercadoMain.java
+++ src/classes/SistemaSupermercadoMain.java
@@ -1,11 +1,19 @@
 package classes;
 import interfaceGrafica.VendaGUI;
 import exceptions.NegativeStockException;
+import exceptions.UsernameNotUniqueException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SistemaSupermercadoMain {
 
     public static void main(String[] args) {
         UsuariosLista usuariosLista = new UsuariosLista();
+        try {
+            usuariosLista.addUsuario("Danilo", "1234", "Admin");
+        } catch (UsernameNotUniqueException ex) {
+            
+        }
         
 	Venda venda = new Venda();
 	VendaGUI vendaWindow = new VendaGUI(venda);
