2d7562d9a4f749e1ce86bf2f094347b7e556adb4
danil <danil@NOTE-DANILO>
2017-12-06 14:57:41 -0200

    Modificado visual para "Windows"

--- src/interfaceGrafica/VendaGUI.java
+++ src/interfaceGrafica/VendaGUI.java
@@ -29,71 +29,12 @@ public class VendaGUI extends javax.swing.JFrame {
      * Creates new form VendaGUI
      */
     public VendaGUI() {
-	/* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-	 * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-	 */
-	
-	try {
-	    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-		if ("Nimbus".equals(info.getName())) {
-		    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-		    break;
-		}
-	    }
-	}
-	catch (ClassNotFoundException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	catch (InstantiationException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	catch (IllegalAccessException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	catch (javax.swing.UnsupportedLookAndFeelException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	
-	//</editor-fold>
 	initComponents();
     }
     
     public VendaGUI(Venda venda) {
-	
-	/* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-	 * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-	 */
-	
-	try {
-	    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-		if ("Nimbus".equals(info.getName())) {
-		    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-		    break;
-		}
-	    }
-	}
-	catch (ClassNotFoundException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	catch (InstantiationException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	catch (IllegalAccessException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	catch (javax.swing.UnsupportedLookAndFeelException ex) {
-	    java.util.logging.Logger.getLogger(VendaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	}
-	
-	//</editor-fold>
 	initComponents();
-	
 	myInitComponents();
-	
 	this.venda = venda;
 	
     }
@@ -322,10 +263,6 @@ public class VendaGUI extends javax.swing.JFrame {
 	    txtfd_total.setText(String.format("R$%4.2f", venda.getTotal()));
 	}
     }//GEN-LAST:event_btn_removeProdutoActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
