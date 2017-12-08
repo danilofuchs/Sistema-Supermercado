e34ae2a14184bc3660e830b11627e1bd88dc2bfe
Brendow Avanzi <Brendow Avanzi@DESKTOP-NMN83KD>
2017-12-08 19:04:05 -0200

    Alterações feitas na classe NotaFiscal para incluir um atributo Empresa

--- src/classes/NotaFiscal.java
+++ src/classes/NotaFiscal.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class NotaFiscal {
     private ArrayList<ItemVenda> itens;
+    private Empresa empresa;
 
-    public NotaFiscal(ArrayList<ItemVenda> itens) {
+    public NotaFiscal(ArrayList<ItemVenda> itens, Empresa empresa) {
         this.itens = itens;
+        this.empresa = empresa;
     }
 
     public ArrayList<ItemVenda> getItens() {
@@ -15,6 +17,14 @@ public class NotaFiscal {
 
     public void setItens(ArrayList<ItemVenda> itens) {
         this.itens = itens;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
     }
     
 }
