a5c614917333028bfd9a11430202c8b5883fa142
Brendow Avanzi <Brendow Avanzi@DESKTOP-NMN83KD>
2017-12-11 16:36:13 -0200

    Mudanças em NotaFiscal: usuario adicionado.

--- src/classes/NotaFiscal.java
+++ src/classes/NotaFiscal.java
@@ -2,16 +2,17 @@ package classes;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class NotaFiscal {
     private Venda venda;
     private Empresa empresa;
+    private Usuario usuario;
 
-    public NotaFiscal(Venda venda, Empresa empresa) {
+    public NotaFiscal(Venda venda, Empresa empresa, Usuario usuario) {
         this.venda = venda;
         this.empresa = empresa;
+        this.usuario = usuario;
     }
 
     public Venda getVenda() {
@@ -28,6 +29,14 @@ public class NotaFiscal {
 
     public void setEmpresa(Empresa empresa) {
         this.empresa = empresa;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
     }
     
     public String getData(){
