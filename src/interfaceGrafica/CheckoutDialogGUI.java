8f89cbff875151e29d217e71acb11578f847301d
danil <danil@NOTE-DANILO>
2017-12-11 18:10:10 -0200

    Reorganizado código e adicionado comentários

--- src/interfaceGrafica/CheckoutDialogGUI.java
+++ src/interfaceGrafica/CheckoutDialogGUI.java
@@ -31,8 +31,8 @@ public class CheckoutDialogGUI extends javax.swing.JDialog {
 	this.venda = venda;
 	this.usuario = usuario;
 	this.cargosLista = cargosLista;
-	pago = new BigDecimal("0.00");
-	troco = new BigDecimal("0.00");
+	pago = new BigDecimal("0.00");  //Preco pago pelo usuario (digitado)
+	troco = new BigDecimal("0.00"); //Troco devido (calculado)
 	initComponents();
 	myInitComponents();
     }
@@ -45,6 +45,9 @@ public class CheckoutDialogGUI extends javax.swing.JDialog {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="My init codes">
     private void myInitComponents() {
+	//Inicializa componentes personalizados
+	
+	//Pega valor total da venda
 	txtfd_total.setText(String.format("%.2f", venda.getTotal()));
 	
 	combo_metodos.addItem("Dinheiro");
@@ -232,35 +235,42 @@ public class CheckoutDialogGUI extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void combo_metodosItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_combo_metodosItemStateChanged
-        String metodoPagamento = (String)combo_metodos.getSelectedItem();
+        //A cada metodo de pagamento selecionado, um comportamento necessário
+	String metodoPagamento = (String)combo_metodos.getSelectedItem();
 	if (metodoPagamento.equals("Dinheiro")) {
+	    //Para dinheiro, pode-se editar o valor livremente
 	    pago = new BigDecimal("0.00");
 	    txtfd_pago.setText("0,00");
 	    txtfd_pago.setEditable(true);
 	} else if (metodoPagamento.equals("Cartão de crédito")) {
+	    //Para cartão, o valor é SEMPRE igual ao valor da venda
 	    pago = venda.getTotal();
 	    txtfd_pago.setText(String.format("%.2f", venda.getTotal().setScale(2)));
 	    txtfd_pago.setEditable(false);
 	} else if (metodoPagamento.equals("Cartão de débito")) {
+	    //Para cartão, o valor é SEMPRE igual ao valor da venda
 	    pago = venda.getTotal();
 	    txtfd_pago.setText(String.format("%.2f", venda.getTotal().setScale(2)));
 	    txtfd_pago.setEditable(false);
 	}
+	//Recalcula o troco devido
 	updateTroco();
     }//GEN-LAST:event_combo_metodosItemStateChanged
 
     private void txtfd_pagoKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtfd_pagoKeyReleased
-        if (txtfd_pago.isEditable()) {
+        //Adiciona o valor pago pelo método específico
+	if (txtfd_pago.isEditable()) {
 	    addValuePago(evt.getKeyChar());
 	}
     }//GEN-LAST:event_txtfd_pagoKeyReleased
 
     private void txtfd_pagoPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_txtfd_pagoPropertyChange
-        updateTroco();
+        //Atualiza troco se o valor pago mudou.
+	updateTroco();
     }//GEN-LAST:event_txtfd_pagoPropertyChange
 
     private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
-        
+        //Se o usuario tentar fechar a janela, pede confirmação
 	int confirmExit = JOptionPane.showConfirmDialog(rootPane, "Deseja editar a venda?", "Editar venda", JOptionPane.YES_NO_OPTION);
 
 	if (confirmExit == JOptionPane.OK_OPTION) {
@@ -271,14 +281,18 @@ public class CheckoutDialogGUI extends javax.swing.JDialog {
     }//GEN-LAST:event_formWindowClosing
 
     private void btn_finalizarVendaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_finalizarVendaActionPerformed
-        if (pago.max(venda.getTotal()).equals(pago)) {
+	if (pago.max(venda.getTotal()).equals(pago)) {
+	    //Só finaliza venda se o valor pago é maior que o valor da venda
 	    Estoque estoque = new Estoque();
+	    //pega estoque do arquivo
 	    estoque.inicializarEstoque();
 	    for (int i = 0; i < venda.getNumItens(); i++) {
+		//Remove quantidade do estoque para cada item da venda
 		Produto prod = venda.getItemVenda(i).getProduto();
 		estoque.removeQtdItem(prod.getCodigoDeBarras(), venda.getItemVenda(i).getQtd());
 	    }
 	    try {
+		//Salva estoque atualizado no arquivo
 		estoque.updateEstoque();
 	    }
 	    catch (IOException ex) {
@@ -287,10 +301,13 @@ public class CheckoutDialogGUI extends javax.swing.JDialog {
 	    
 	    
 	    try {
-		Empresa empresa;
-		empresa = Empresa.empresaDeArquivo();
-		NotaFiscal nota = new NotaFiscal(venda, empresa);
+		//Pega dados da empresa em um arquivo
+		Empresa empresa = Empresa.empresaDeArquivo(); 
+		//Cria uma nova nota fiscal para a Empresa e esta Venda
+		NotaFiscal nota = new NotaFiscal(venda, empresa, usuario);
+		//Mostra esta nota no textArea
 		txtarea_notaFiscal.setText(nota.imprimirNota());
+		//Salva esta nota num arquivo
 		nota.salvaNotaArquivo();
 	    }
 	    catch (IOException ex) {
@@ -298,28 +315,39 @@ public class CheckoutDialogGUI extends javax.swing.JDialog {
 	    }
 
 	} else {
+	    //Não pode finalizar venda com pagamento menor que o total da venda
 	    JOptionPane.showMessageDialog(rootPane, "O valor pago é menor que o valor da venda","",JOptionPane.ERROR_MESSAGE);
 	}
     }//GEN-LAST:event_btn_finalizarVendaActionPerformed
 
     private void txtfd_pagoKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtfd_pagoKeyPressed
-        if (evt.getKeyCode() == Event.ENTER) {
+        //Caso clique ENTER em txtfd_pago, clica o botão de finalizar venda
+	if (evt.getKeyCode() == Event.ENTER) {
 	    btn_finalizarVenda.doClick();
 	}
     }//GEN-LAST:event_txtfd_pagoKeyPressed
 
     private void updateTroco() {
+	//Recalcula troco e grava na tela
 	troco = pago.subtract(venda.getTotal());
 	txtfd_troco.setText(String.format("%.2f", troco));
     }
     
     private void addValuePago(char key) {
+	// Esse método vai adicionar o caracter ao final do BigDecimal,
+	// garantindo duas casas decimais
 	if (key >= '0' && key <= '9') {
+	    //adiciona digito novo 100 vezes menor
+	    //	Exemplo:     Valor antigo = 10,00
+	    //	 Digitou 3   Valor novo	 = 100,03
 	    pago = pago.multiply(new BigDecimal("10"));
 	    pago = pago.add(new BigDecimal(String.valueOf(key)).scaleByPowerOfTen(-2));
 	} else if (key == KeyEvent.VK_BACK_SPACE) {
+	    //Se quer deletar um digito, deve zerar a casa decimal 2 (setScale(1)),
+	    //dividir o valor por 10 e reabrir a possibilidade da 2a casa decimal
 	    pago = pago.setScale(1,RoundingMode.DOWN).divide(new BigDecimal("10")).setScale(2);
 	}
+	//Mostra o novo valor onde a pessoa está digitando e atualiza o troco
 	txtfd_pago.setText(String.format("%.2f", pago));
 	updateTroco();
     }
