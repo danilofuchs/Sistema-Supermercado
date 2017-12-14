81921ee4ef0c44c7dfd515a6db98060c7bd86608
Brendow Avanzi <Brendow Avanzi@DESKTOP-NMN83KD>
2017-12-14 19:19:13 -0200

    NotaFiscal

--- src/classes/NotaFiscal.java
+++ src/classes/NotaFiscal.java
@@ -1,7 +1,13 @@
 package classes;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class NotaFiscal {
@@ -45,10 +51,32 @@ public class NotaFiscal {
 	return dateFormat.format(date);
     }
     
-    public String imprimirNota(){
-        return "";
-    }
-    public void salvaNotaArquivo(){
+    public ArrayList<String> imprimirNota() throws IOException{
+        salvaNotaArquivo();
+        ArrayList<String> notaArquivo = new ArrayList();
+        notaArquivo = (ArrayList) Files.readAllLines(Paths.get("NotaFiscal.txt"));
         
+        return notaArquivo;
+    }
+    public void salvaNotaArquivo() throws IOException{
+        ArrayList<String> notaArray = new ArrayList();
+        String s = String.format("CNPJ: " + empresa.getCnpj() + " " + 
+                empresa.getNome() + "\n" + empresa.getEndereco() + "\n\n" + 
+                "Item  Codigo  Descrição  Qtd  Unit  Total");
+        notaArray.add(s);
+        for(ItemVenda v: venda.getItens()){
+            s = String.format(v.getSequencial() + v.getProduto().getCodigoDeBarras() + "  " + 
+                    v.getProduto().getNome() + "\n   " + v.getQtd() + "x" + v.getProduto().getUnidade() + 
+                    v.getProduto().getPreco().toString() + "  " + v.getPrecoTotalItem().toString());
+            notaArray.add(s);
+        }
+        s = String.format("-------------------------------------------------------\n" + "Qtd. total de itens" + 
+                "                                  " + venda.getNumItens() + "\n" + "Valor total R$" + 
+                "                                   " + venda.getTotal() + 
+                "-------------------------------------------------------\n" + "Operador: " + 
+                usuario.getNome() + " (" + usuario.getCargo() + ")");
+        notaArray.add(s);
+        
+        Files.write(Paths.get("NotaFiscal.txt"), notaArray, StandardCharsets.UTF_8, StandardOpenOption.TRUNCATE_EXISTING);
     }
 }
